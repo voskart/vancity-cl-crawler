022d675152cfafcd00965710756437ba301ee168
ginetrix <voskobojnikov.artemij@gmail.com>
2020-10-11 10:14:03 -0700

    adding apts to a dictionary

--- cl_housing.py
+++ cl_housing.py
@@ -1,8 +1,10 @@
 import requests
+import logging
+from bs4 import BeautifulSoup
 
 class Housing:
 
-    url = None
+    results = {}
 
     filters = {
         'search_distance': None,
@@ -12,9 +14,28 @@ class Housing:
     }
 
     def __init__(self, city=None, category=None, filters=None):
+        logging.basicConfig(level=logging.INFO)
         self.url = "https://{0}.craigslist.org/search/{1}".format(city, category)
         self.filters = filters
 
     def get_apartments(self):
         response = requests.get(self.url, params=self.filters)
-        return response.content
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # find all results
+        rows = soup.find_all('li', {'class': 'result-row'})
+        self.process_rows(rows)
+        self.get_apt_number(soup)
+
+    def process_rows(self, rows):
+    	apt = None
+    	for row in rows:
+    		id = row.attrs['data-pid']
+    		link = row.find('a')['href']
+    		price = row.find('span', {'class': 'result-price'})
+    		neighborhood = row.find('span', {'class': 'result-hood'})
+    		date = row.find('time')['datetime']
+    		self.results[id] = {'link': link, 'price': price.text, 'neighborhood': neighborhood.text, 'date': date}
+
+    def get_apt_number(self, soup):
+        total = soup.find('span', {'class': 'totalcount'})
+        logging.info('Total found apartments: %i' % int(total.text))
--- crawl_apts.py
+++ crawl_apts.py
@@ -1,10 +1,11 @@
 from mailservice import send_mail
 from cl_housing import Housing
+import logging
+
 
 def main():
-    # send_mail('test')
-    apt_van = Housing(city='vancouver', category='apa', filters={'max_price': 1800, 'min_bedrooms': 2, 'search_distance': 5.5, 'zip_code':'V5Z+1M9'})
-    print(apt_van.get_apartments())
+    apt_van = Housing(city='vancouver', category='apa', filters={'max_price': 1800, 'min_bedrooms': 2, 'search_distance': 6, 'postal':'V6H1T4', 'postedToday':1})
+    apt_van.get_apartments()
 
 if __name__ == '__main__':
 	main()
